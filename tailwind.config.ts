4bf6f8115e97042d04abddae543f1d52d1b661f4
JaydethSp01 <132963442+JaydethSp01@users.noreply.github.com>
2026-06-08 18:10:18 -0500

    feat: ScrumDev AI build :: tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,1 @@
+module.exports = { darkMode: "class", content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'], theme: { extend: { colors: { brand: { DEFAULT: "#059669", dark: "#036c4b" }, },} }, plugins: [] };
